900d1eae606b7491d3ce1f2f569bf5f842cc2c0e
Cody-Keasling-LAS1DA <132222567+Cody-Keasling-LAS1DA@users.noreply.github.com>
2023-11-11 12:16:41 -0800

    setup gh-pages

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import "./index.css"; // Import your index.css file here
 // Wrap the App component with BrowserRouter and set the basename
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename="/project-15/movie-app">
+    <BrowserRouter basename="movie-app">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
@@ -17,3 +17,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+//dont need
